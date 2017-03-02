a652be3150376dee10d84e0cdb2fd0e00bf74500
wangJun-Ocean <1377162122@qq.com>
2017-03-02 17:51:40 +0800

    Update ViewController.m

--- WJTestOne/ViewController.m
+++ WJTestOne/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [self rangTest:@"nsmj2"];
+    NSLog(@"123");
     [self testRange];
 }
 
